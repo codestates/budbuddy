0b35b19b36b48c886f6f03f0ff38deb314698fe6
HyesungYoon <yhs9421877@gmail.com>
2022-03-18 18:01:50 +0900

    Feat: express로깅 및 환경변수 설정
    morgan을 이용한 서버 http요청들 로깅 및 NODE_ENV환경변수에 따른 서버 셋팅 분기 구현하였습니다.

--- server/index.js
+++ server/index.js
@@ -1,34 +1,53 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+var rfs = require("rotating-file-stream");
+
+// create a rotating write stream
+var accessLogStream = rfs.createStream("access.log", {
+  interval: "1d", // rotate daily
+  path: path.join(__dirname, "log"),
+});
 
 const app = express();
-const PORT = process.env.SERVERPORT || 80;
+const PORT = process.env.SERVER_PORT || 3000;
 
-app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: [process.env.REACT_APP_API_URL],
-    credentials: true,
-    methods: ["GET", "POST", "OPTIONS", "DELETE", "PUT"],
-  }),
-);
+
+let corsOrigin = "*";
+
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+  app.use(morgan("dev"));
+  corsOrigin = `http://localhost:${PORT}`;
+} else if (process.env.NODE_ENV === "production") {
+  app.use(morgan("common", { stream: accessLogStream }));
+  corsOrigin = ["https://budbuddy.click", "http://budbuddy.click"];
+}
+
+const corsOptions = {
+  origin: corsOrigin,
+  credentials: true,
+  methods: ["GET", "POST", "OPTIONS", "DELETE", "PUT"],
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("버드버디! 시작!");
 });
 
+//router
 const controllers = require("./controllers");
 app.post("/login", controllers.login);
 
 app.listen(PORT, () => {
-  console.log(`서버 시작 http://localhost:${PORT}`);
+  console.log(`서버 시작 ${corsOrigin}`);
 });
 
 module.exports = app;
