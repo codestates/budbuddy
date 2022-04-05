af71b4e3a51939799e9f50e4b756e9ced2c92c67
parkjeyoung <dkfmwpsxm@naver.com>
2022-04-06 02:06:40 +0900

    Chore: 홈에쓰이는 더미데이터 json으로 변경

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { slideImgs, proverbs } from "../utils/dummy";
 import TextOnImg from "../components/common/TextOnImg";
 import Hr from "../components/common/Hr";
 import ImgSlide from "../components/common/ImgSlide";
+import axios from "axios";
 
-export const Layout = styled.div`
+const Layout = styled.div`
   padding-top: 5rem;
 
   .greeting {
@@ -15,12 +15,25 @@ export const Layout = styled.div`
 `;
 
 const Home = () => {
+  useEffect(() => {
+    read();
+  }, []);
+
+  const [data, setData] = useState(null);
+
+  async function read() {
+    const { data } = await axios.get("Dummy/dummy.json");
+    console.log(data);
+
+    setData(data);
+  }
+
   return (
     <Layout>
       <p className="greeting">{`안녕하세요!\n자신의 식물을 일기처럼 기록해보세요!`}</p>
-      <TextOnImg texts={proverbs} />
+      <TextOnImg texts={data.proverbs} />
       <Hr t={4} b={4} width={80} />
-      <ImgSlide images={slideImgs} />
+      <ImgSlide images={data.slide} />
     </Layout>
   );
 };
