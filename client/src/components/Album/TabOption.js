f38cfbbfa84a1ae381ab78efc98e61a9073f845b
applehole <skyheo123@naver.com>
2022-04-07 23:19:00 +0900

    Feat Fix Build Chore CI Docs Style Refactor Test: 제목
    무엇을
    왜
    어떻게

--- client/src/components/Album/TabOption.js
+++ client/src/components/Album/TabOption.js
@@ -42,7 +42,7 @@ export const Layout = styled.div`
   }
 `;
 
-const TabOption = ({ className = "", tabName, setPickPlantValue, setPickDateValue, listByUserId }) => {
+const TabOption = ({ className = "", tabName, setPickPlantValue, setPickDateValue, listByUserId, currentUser = "" }) => {
   const UserPlantsName = listByUserId.reduce((acc, cur) => {
     if (!acc.includes(cur.Plant.name) && cur.Journal_Images.length !== 0) {
       acc.push(cur.Plant.name);
@@ -75,7 +75,7 @@ const TabOption = ({ className = "", tabName, setPickPlantValue, setPickDateValu
               return <option key={el}>{el}</option>;
             })}
           </select>
-          <select defaultValue="식물이름" className="rightcap" onChange={(e) => OptionPlantValue(e)}>
+          <select defaultValue={currentUser || "식물이름"} className="rightcap" onChange={(e) => OptionPlantValue(e)}>
             <option>식물이름</option>
             {UserPlantsName.map((el) => {
               return <option key={el}>{el}</option>;
